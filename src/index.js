3b5d491867c51ca1aa6fb017102628d451d18ddc
Michael Pagaran <mmpagaran64@gmail.com>
2022-06-16 15:06:01 +0800

    added states

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={function() {
-          console.log('click');
-        }}>
+        <button className="square" 
+          onClick={() => this.props.onClick()}
+        >
           {this.props.value}
         </button>
       );
@@ -15,8 +15,19 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+    
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+        <Square value={this.state.squares[i]}
+        onClick={() => this.handeClick(i)}
+      />
+      );
     }
   
     render() {
